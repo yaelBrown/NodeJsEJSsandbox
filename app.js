02d126f60fe90d30e1b54e1ef7cccdfc314d8e1a
Yael Brown <yaelrbrown@gmail.com>
2019-11-15 22:20:53 -0600

    Created working examples of loops, includes with templating, and passing route parameters

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("cool"));
+
+app.set('view engine', 'ejs');
+
+// app.get("/", (req, res) => res.render("index.ejs"));
+
+// passing in a variable
+app.get("/printName/:name", (req, res) => {
+  const name = req.params.name;
+  res.render("index.ejs", {
+    title: name
+  });
+});
+
+app.get("/loops", (req, res) => {
+  arr = ["yael", "is", "really", "cool"];
+
+  res.render("loops.ejs", {
+    data: arr
+  });
+});
+
+app.get("/templates", (req, res) => res.render("template.ejs"));
+
+
 app.listen("3000", () => console.log("listening on port 3000"));
